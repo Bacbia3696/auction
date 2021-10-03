0cb12300dfaa107367c845f1d3833585297d8a10
huongnq2 <quanghuongitus@gmail.com>
2021-10-04 00:40:18 +0700

    add support function

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bacbia3696/auction
 go 1.16
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/locales v0.14.0
 	github.com/go-playground/universal-translator v0.18.0
@@ -13,10 +14,11 @@ require (
 	github.com/lib/pq v1.10.3
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/nyaruka/phonenumbers v1.0.71 // indirect
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/viper v1.9.0
 	github.com/ugorji/go v1.2.6 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
 	golang.org/x/sys v0.0.0-20211002104244-808efd93c36d // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
--- internal/server/user.go
+++ internal/server/user.go
@@ -1,12 +1,20 @@
 package server
 
 import (
+	"golang.org/x/crypto/bcrypt"
+	"html"
+	"net"
 	"net/http"
+	"regexp"
+	"strings"
 
 	db "github.com/bacbia3696/auction/db/sqlc"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/copier"
 	"github.com/sirupsen/logrus"
+	"github.com/asaskevich/govalidator"
+
+
 )
 
 type createUserRequest struct {
@@ -16,10 +24,43 @@ type createUserRequest struct {
 	// Email    string `json:"email" binding:"required,email"`
 }
 
+func HashPassword(password string) string {
+	bytes, _ := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes)
+}
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+func Santize(data string) string{
+	data = html.EscapeString(strings.TrimSpace(data))
+	return data
+}
+var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+func isEmailValid(e string) bool {
+	if len(e) < 3 && len(e) > 254 {
+		return false
+	}
+	if !emailRegex.MatchString(e) {
+		return false
+	}
+	parts := strings.Split(e, "@")
+	mx, err := net.LookupMX(parts[1])
+	if err != nil || len(mx) == 0 {
+		return false
+	}
+	return true
+}
+
 func (s *Server) createUser(ctx *gin.Context) {
 	var req createUserRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		responeErr(ctx, err, 1)
+		return
+	}
+	if govalidator.IsNull(req.UserName) {
+		responeErr(ctx, nil, 1)
 		return
 	}
 	// create new user
