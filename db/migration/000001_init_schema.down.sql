19c241e5ad5e3ac5b38a404e899ceac30f02003f
DatNguyen <dat.nguyen1@be.com.vn>
2021-10-09 22:54:59 +0700

    fix schema down

--- db/migration/000001_init_schema.down.sql
+++ db/migration/000001_init_schema.down.sql
@@ -1,4 +1,8 @@
-drop table users;
-drop table user_images;
-drop table role;
-drop table user_role;
+drop table if exists users;
+drop table if exists user_images;
+drop table if exists role;
+drop table if exists user_role;
+drop table if exists auctions;
+drop table if exists auction_images;
+drop table if exists register_auction;
+drop table if exists register_auction_images;
